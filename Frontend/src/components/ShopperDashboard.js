c6ad406482c831bf7a6f02494ceef29d4774d3a6
nkosbonileMteto <2553616@sudents.wits.ac.za>
2024-04-23 20:06:48 +0200

    Added a logout button for shopper dashboard

--- Frontend/src/components/ShopperDashboard.js
+++ Frontend/src/components/ShopperDashboard.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 function ShopperDashboard() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Perform logout actions, such as clearing authentication tokens or session data
+    // For simplicity, let's just navigate back to the login page
+    navigate('/');
+};
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '48px' }}>
+    <div style={{display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <h1>SHOPPER DASHBOARD</h1>
+      <button className="logout-button" onClick={handleLogout}>Logout</button>
     </div>
   );
 }
